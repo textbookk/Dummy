08782353881a0b90452a858e3cf2bc043ce4928b
maroti <100464574+textbookk@users.noreply.github.com>
2025-11-25 17:54:11 +0530

    aa

---
+++ src/test/java/com/seroter/azure_basic_app/AzureBasicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seroter.azure_basic_app;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureBasicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
